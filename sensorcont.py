bd0d590666b774b87049187f8529606709695f54
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-01-29 13:21:01 -0600

    added left control

--- sensorcont.py
+++ sensorcont.py
@@ -6,6 +6,13 @@ left_sensor = 18
 rt_sensor = 17
 rt_wheel = 2
 lft_wheel = 1
+forward = 1000
+backward = 2000
 RPL.servoWrite(0,0)
-command =
-RPL.servoWrite(
+command = raw_input("> ")
+if command == "left":
+    direction = raw_input("> ")
+    if direction == "forward":
+        RPL.servoWrite(lft_wheel,forward)
+    elif direction == "backward":
+        RPL.servoWrite(lft_wheel,backward)
