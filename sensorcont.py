e483acc39be765864e9c64cfba751df955c2815a
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-01-30 13:31:48 -0600

    made into a function

--- sensorcont.py
+++ sensorcont.py
@@ -3,65 +3,68 @@ from setup import RPL
 import post_to_web as PTW
 def retrn():
     exit(0)
-back_sensor = 16
-left_sensor = 18
-rt_sensor = 17
-rt_wheel = 0
-lft_wheel = 1
-forward = 1000
-backward = 2000
-RPL.servoWrite(0,0)
-command = raw_input("> ")
-if command == "left":
-    direction = raw_input("> ")
-    if direction == "forward":
-        RPL.servoWrite(lft_wheel,forward)
-        followup = raw_input("> ")
-        if followup == "stop":
-            RPL.servoWrite(lft_wheel,0)
-            RPL.servoWrite(rt_wheel,0)
-            retrn()
-    elif direction == "backward":
-        RPL.servoWrite(lft_wheel,backward)
-        followup = raw_input("> ")
-        if followup == "stop":
-            RPL.servoWrite(lft_wheel,0)
-            RPL.servoWrite(rt_wheel,0)
-            retrn()
-elif command == "right":
-    direction = raw_input("> ")
-    if direction == "forward":
-        RPL.servoWrite(rt_wheel,forward)
-        followup = raw_input("> ")
-        if followup == "stop":
-            RPL.servoWrite(lft_wheel,0)
-            RPL.servoWrite(rt_wheel,0)
-            retrn()
-    elif direction == "backward":
-        RPL.servoWrite(rt_wheel,backward)
-        followup = raw_input("> ")
-        if followup == "stop":
-            RPL.servoWrite(lft_wheel,0)
-            RPL.servoWrite(rt_wheel,0)
-            retrn()
-elif command == "both":
-    direction = raw_input("> ")
-    if direction == "forward":
-        RPL.servoWrite(lft_wheel,backward)
-        RPL.servoWrite(rt_wheel,forward)
-        followup = raw_input("> ")
-        if followup == "stop":
-            RPL.servoWrite(lft_wheel,0)
-            RPL.servoWrite(rt_wheel,0)
-            retrn()
-    elif direction == "backward":
-        RPL.servoWrite(rt_wheel,backward)
-        RPL.servoWrite(lft_wheel,forward)
-        followup = raw_input("> ")
-        if followup == "stop":
-            RPL.servoWrite(lft_wheel,0)
-            RPL.servoWrite(rt_wheel,0)
-            retrn()
-elif command == "stop":
-    RPL.servoWrite(lft_wheel,0)
-    RPL.servoWrite(rt_wheel,0)
+def usercont():
+    back_sensor = 16
+    left_sensor = 18
+    rt_sensor = 17
+    rt_wheel = 0
+    lft_wheel = 1
+    forward = 1000
+    backward = 2000
+    RPL.servoWrite(0,0)
+    command = raw_input("> ")
+    if command == "left":
+        direction = raw_input("> ")
+        if direction == "forward":
+            RPL.servoWrite(lft_wheel,forward)
+            followup = raw_input("> ")
+            if followup == "stop":
+                RPL.servoWrite(lft_wheel,0)
+                RPL.servoWrite(rt_wheel,0)
+                retrn()
+        elif direction == "backward":
+            RPL.servoWrite(lft_wheel,backward)
+            followup = raw_input("> ")
+            if followup == "stop":
+                RPL.servoWrite(lft_wheel,0)
+                RPL.servoWrite(rt_wheel,0)
+                retrn()
+    elif command == "right":
+        direction = raw_input("> ")
+        if direction == "forward":
+            RPL.servoWrite(rt_wheel,forward)
+            followup = raw_input("> ")
+            if followup == "stop":
+                RPL.servoWrite(lft_wheel,0)
+                RPL.servoWrite(rt_wheel,0)
+                retrn()
+        elif direction == "backward":
+            RPL.servoWrite(rt_wheel,backward)
+            followup = raw_input("> ")
+            if followup == "stop":
+                RPL.servoWrite(lft_wheel,0)
+                RPL.servoWrite(rt_wheel,0)
+                retrn()
+    elif command == "both":
+        direction = raw_input("> ")
+        if direction == "forward":
+            RPL.servoWrite(lft_wheel,backward)
+            RPL.servoWrite(rt_wheel,forward)
+            followup = raw_input("> ")
+            if followup == "stop":
+                RPL.servoWrite(lft_wheel,0)
+                RPL.servoWrite(rt_wheel,0)
+                retrn()
+        elif direction == "backward":
+            RPL.servoWrite(rt_wheel,backward)
+            RPL.servoWrite(lft_wheel,forward)
+            followup = raw_input("> ")
+            if followup == "stop":
+                RPL.servoWrite(lft_wheel,0)
+                RPL.servoWrite(rt_wheel,0)
+                retrn()
+    elif command == "stop":
+        RPL.servoWrite(lft_wheel,0)
+        RPL.servoWrite(rt_wheel,0)
+
+usercont()
