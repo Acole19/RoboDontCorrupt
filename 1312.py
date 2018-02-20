d4024466756efef4ad354753cd3829ad8bfbe0e0
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-02-20 13:28:39 -0600

    fixed indentation syntax

--- 1312.py
+++ 1312.py
@@ -8,12 +8,12 @@ def div5():
 
 while True:
     if div3() == 0 and div5() == 0:
-      RPL.servoWrite(1,1000)
-      RPL.servoWrite(0,2000)
-  elif div3() == 0 and div5() != 0:
-      RPL.servoWrite(1,2000)
-  elif div3() != 0 and div5 == 0:
-      RPL.servoWrite(0,1000)
-  else:
-      RPL.servoWrite(0,0)
-      RPL.servoWrite(1,0)
+        RPL.servoWrite(1,1000)
+        RPL.servoWrite(0,2000)
+    elif div3() == 0 and div5() != 0:
+        RPL.servoWrite(1,2000)
+    elif div3() != 0 and div5 == 0:
+        RPL.servoWrite(0,1000)
+    else:
+        RPL.servoWrite(0,0)
+        RPL.servoWrite(1,0)
