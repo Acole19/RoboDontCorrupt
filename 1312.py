a640fd4333c35de5e1c2847e45a28b0b38150dae
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-02-20 13:23:43 -0600

    added motor control

--- 1312.py
+++ 1312.py
@@ -2,7 +2,18 @@ import time
 import setup
 from setup import RPL
 def div3:
-  time.time
+    return time.time() % 3
+def div5:
+    return time.time() % 5
 
 while True:
-  if time.time() % 3 = 0
+    if div3() == 0 and div5() == 0:
+      RPL.servoWrite(1,1000)
+      RPL.servoWrite(0,2000)
+    elif div3() == 0 and div5() != 0:
+      RPL.servoWrite(1,2000)
+  elif div3() != 0 and div5 == 0:
+      RPL.servoWrite(0,1000)
+  else:
+      RPL.servoWrite(0,0)
+      RPL.servoWrite(1,0)
