f0b4465acbca0bfa6a6271bea51e853327b0001e
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-02-20 13:29:34 -0600

    fixed loop

--- 1312.py
+++ 1312.py
@@ -14,6 +14,3 @@ while True:
         RPL.servoWrite(1,2000)
     elif div3() != 0 and div5 == 0:
         RPL.servoWrite(0,1000)
-    else:
-        RPL.servoWrite(0,0)
-        RPL.servoWrite(1,0)
