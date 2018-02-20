032191bcfd33b5d1770a8bbf39096b6c922f9b8a
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-02-20 13:42:11 -0600

    shortened conditons

--- 1312.py
+++ 1312.py
@@ -12,12 +12,12 @@ def div5():
     return g
 
 while True:
-    if div3() == 0 and div5() == 0:
-        RPL.servoWrite(1,1000)
-        RPL.servoWrite(0,2000)
-    elif div3() == 0 and div5() != 0:
+    y = z - time.time()
+    if y % 4 == 0:
         RPL.servoWrite(1,2000)
-    elif div3() != 0 and div5 == 0:
         RPL.servoWrite(0,1000)
+    elif y % 8 == 0:
+        RPL.servoWrite(1,0)
+        RPL.servoWrite(0,0)
 RPL.servoWrite(0,0)
 RPL.servoWrite(1,0)
