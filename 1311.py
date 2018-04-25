f935aa71b5daa319621629b6049f2e151f1d9c87
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-04-25 13:48:51 -0500

    2

--- 1311.py
+++ 1311.py
@@ -6,10 +6,10 @@ def Distance():
     return RPL.readDistance(16)
 def motorForw():
     RPL.servoWrite(1,2000)
-    RPL.servoWrite(0,1000)
+    RPL.servoWrite(2,1000)
 def motorstop():
     RPL.servoWrite(1,000)
-    RPL.servoWrite(0,000)
+    RPL.servoWrite(2,000)
 def rearreasp():
     back_sensor = 16
     rt_wheel = 0
