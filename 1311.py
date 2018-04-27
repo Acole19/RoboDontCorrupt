bf17bf19c5e67f338ff93cb3211c1bab5d045678
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-04-27 13:17:14 -0500

    added left and right

--- 1311.py
+++ 1311.py
@@ -10,6 +10,12 @@ def motorForw():
 def motorstop():
     RPL.servoWrite(1,000)
     RPL.servoWrite(2,000)
+def motorLeft():
+    RPL.servoWrite(1,000)
+    RPL.servoWrite(2,1000)
+def motorRight():
+    RPL.servoWrite(1,1000)
+    RPL.servoWrite(2,000)
 def rearreasp():
     back_sensor = 16
     rt_wheel = 0
@@ -20,10 +26,12 @@ def rearreasp():
     while condit != 16.5:
         motorstop()
         Distance()
-        while Distance() < 50000:
+        while Distance() < 50000 and Distance() > 10000:
             motorForw()
         while Distance() > 50000:
-            motorstop()
+            motorLeft()
+        while Distance() > 10000:
+            motorRight
         if RPL.readDistance(17) < 1000:
             condit = 16.5
     motorstop()
