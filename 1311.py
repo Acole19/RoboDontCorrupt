3b7a47c82e33349f8586ea5cdb90b5a46139b06a
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-04-27 13:21:56 -0500

    removed sep def

--- 1311.py
+++ 1311.py
@@ -16,23 +16,21 @@ def motorLeft():
 def motorRight():
     RPL.servoWrite(1,1000)
     RPL.servoWrite(2,000)
-def rearreasp():
-    back_sensor = 16
-    rt_wheel = 0
-    lft_wheel = 1
-    forward = 1000
-    backward = 2000
-    condit = 7
-    while condit != 16.5:
-        motorstop()
-        Distance()
-        while Distance() < 50000 and Distance() > 10000:
-            motorForw()
-        while Distance() > 50000:
-            motorLeft()
-        while Distance() > 10000:
-            motorRight
-        if RPL.readDistance(17) < 1000:
-            condit = 16.5
+back_sensor = 16
+rt_wheel = 0
+lft_wheel = 1
+forward = 1000
+backward = 2000
+condit = 7
+while condit != 16.5:
     motorstop()
-rearreasp()
+    Distance()
+    while Distance() < 50000 and Distance() > 10000:
+        motorForw()
+    while Distance() > 50000:
+        motorLeft()
+    while Distance() > 10000:
+        motorRight
+    if RPL.readDistance(17) < 1000:
+        condit = 16.5
+motorstop()
